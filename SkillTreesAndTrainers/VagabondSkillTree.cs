df28a287e0f57c145a8f2b30a3de558ea2826842
Eivind <ehaugw@gmail.com>
2024-07-16 01:12:01 +0200

    skiill tree: add command beast to skill tree

--- SkillTreesAndTrainers/VagabondSkillTree.cs
+++ SkillTreesAndTrainers/VagabondSkillTree.cs
@@ -61,11 +61,12 @@ namespace Vagabond
                     } },
 
                     new SL_SkillRow() { RowIndex = 4, Slots = new List<SL_BaseSkillSlot>() {
+                        new SL_SkillSlot() { ColumnIndex = 2, SilverCost = 600, SkillID = IDs.tameBeastID,                  Breakthrough = false,   RequiredSkillSlot = new Vector2(3,2) },
                     } },
 
                     new SL_SkillRow() { RowIndex = 5, Slots = new List<SL_BaseSkillSlot>() {
                         new SL_SkillSlot() { ColumnIndex = 1, SilverCost = 600, SkillID = IDs.precisionStrikeSkillID,       Breakthrough = false,   RequiredSkillSlot = new Vector2(3,2) },
-                        new SL_SkillSlot() { ColumnIndex = 2, SilverCost = 600, SkillID = IDs.tameBeastID,                  Breakthrough = false,   RequiredSkillSlot = new Vector2(3,2) },
+                        new SL_SkillSlot() { ColumnIndex = 2, SilverCost = 600, SkillID = IDs.commandBeastSkillID,          Breakthrough = false,   RequiredSkillSlot = new Vector2(4,2) },
                         new SL_SkillSlot() { ColumnIndex = 3, SilverCost = 600, SkillID = IDs.carefulMaintenanceSkillID,    Breakthrough = false,   RequiredSkillSlot = new Vector2(3,2) },
                     } },
                 }
