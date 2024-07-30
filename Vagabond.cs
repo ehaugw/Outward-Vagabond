c31ef0dc3e9cfcb0c826198d5d9890e87e755cce
Eivind <ehaugw@gmail.com>
2024-07-30 17:21:47 +0200

    version bump 0.0.7

--- Vagabond.cs
+++ Vagabond.cs
@@ -17,7 +17,7 @@ namespace Vagabond
     public class Vagabond : BaseUnityPlugin
     {
         public const string GUID = "com.ehaugw.vagabond";
-        public const string VERSION = "0.0.6";
+        public const string VERSION = "0.0.7";
         public const string NAME = "Vagabond";
         public static string ModFolderName = Directory.GetParent(typeof(Vagabond).Assembly.Location).Name.ToString();
 
